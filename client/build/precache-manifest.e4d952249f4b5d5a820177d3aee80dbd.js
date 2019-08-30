8a4501d847a8a25a062367a82f910a4953a2801b
michaeldsdomingo <michael.ds.domingo@gmail.com>
2019-08-30 16:40:36 -0700

    removed build from gitignore

---
+++ client/build/precache-manifest.e4d952249f4b5d5a820177d3aee80dbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edfcc136f0217adb0efe56434772fd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f65a7462f2ca20a16c53",
+    "url": "/static/css/main.b49e2e03.chunk.css"
+  },
+  {
+    "revision": "e2abccd0aa5d94f3aa5f",
+    "url": "/static/js/2.d5ae7d76.chunk.js"
+  },
+  {
+    "revision": "f65a7462f2ca20a16c53",
+    "url": "/static/js/main.f83ed223.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
