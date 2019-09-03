e78c6b336c2ac9883b16959e7fdefbf20415069a
michaeldsdomingo <michael.ds.domingo@gmail.com>
2019-09-02 23:30:17 -0700

    title2

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,7 +11,7 @@ class Navbar extends Component {
                 <div id="navbar">
                     <ul>
                         <li id='title'>
-                            <Link to="/" className="removeUnderline colorWhite">Comic Book Blogspot</Link>
+                            <Link to="/" className="removeUnderline colorWhite">Comic Book Blogspot2</Link>
                         </li>
                         <div className='align-right'>
                             <li>
